801cfa977b02f6562bb1951910f40034d47a807b
Victor <viclee02@gmail.com>
2016-05-17 21:44:22 -1000

    adds project

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+  var myName = 'Jon';
+
+  // function saySomeonesName(name, elementId){
+  //   document.getElementById(elementId).innerHTML = name;
+  // }
+
+  // saySomeonesName(myName, 'myName');
+
+  //THIS IS JUST AN EXAMPLE OF HOW TO CREATE ELEMENTS
+  function generateDocument(){
+    //1. document.createElement
+    //2. element.appendChild
+
+    var element = document.createElement('div');
+    element.id = 'someId';
+    document.body.appendChild(element);
+    //<div id='someId'>TEST</div>
+
+    var newElement = document.createElement('span');
+    newElement.id = 'someOtherId';
+    document.getElementById('someId').appendChild(newElement);
+  }
+
+  generateDocument();
+};
